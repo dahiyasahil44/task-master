ac29b6ed77a9ca653177fb0ca07410983883ab18
dahiyasahil44 <imsahil440407@gmail.com>
2025-08-25 22:37:54 +0530

    Updated App.js file

--- server/app.js
+++ server/app.js
@@ -22,18 +22,25 @@ startReminders();
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
+app.get('/', (req, res) => {
+  res.send('API is running...'); 
+});
+
+app.use((req, res) => {
+  res.send("Page not found!!")
+});
 
 // protected route example
-const auth = require('./middlewares/auth');
-const role = require('./middlewares/role');
+// const auth = require('./middlewares/auth');
+// const role = require('./middlewares/role');
 
-app.get('/api/protected', auth, (req, res) => {
-  res.json({ message: `Welcome ${req.user.name}, you are authenticated!` });
-});
+// app.get('/api/protected', auth, (req, res) => {
+//   res.json({ message: `Welcome ${req.user.name}, you are authenticated!` });
+// });
 
-app.get('/api/admin', auth, role(['admin']), (req, res) => {
-  res.json({ message: `Hello Admin ${req.user.name}` });
-});
+// app.get('/api/admin', auth, role(['admin']), (req, res) => {
+//   res.json({ message: `Hello Admin ${req.user.name}` });
+// });
 
 // start server
 // const PORT = process.env.PORT || 3000;
