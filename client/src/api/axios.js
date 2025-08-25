207c63eea0063d2abedfe7ed4f983ab13071ac89
dahiyasahil44 <imsahil440407@gmail.com>
2025-08-25 23:09:12 +0530

    Backend Server Url updated in frontend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // backend base URL
+  baseURL: "https://task-master-omega-gilt.vercel.app/api", // backend base URL
+  // baseURL: "http://localhost:3000/api", // backend base URL
+  
 });
 
 // Attach JWT token automatically
